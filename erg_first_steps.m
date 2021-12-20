1d5e581dbb537b4c9ae29c266ef5b56f8f9049fe
Jonathan Hanson <jonathan.hanson@colorado.edu>
2021-12-20 12:16:13 -0700

    Latest update up to friday 12/18 email

--- erg_first_steps.m
+++ erg_first_steps.m
@@ -3,34 +3,30 @@ close all; clc; clear
 %
 % Time
 %
-Ts = 0.1;
+Ts = 0.01;
 TT = 10;
-
 
 %
 % System Model
 %
 
-% %1d double int
-% A = [0,1;0,0];
-% B = [0;1];
-% C = [1,0];
-% D = [0];
-% sys = ss(A,B,C,D);
-
 %2d double int with 2 ctrls
-A = [0,0,1,0
-     0,0,0,1
-     0,0,0,0
-     0,0,0,0];
-B = [0,0
-     0,0
-     1,0
-     0,1];
-C = [1,0,0,0
-     0,1,0,0];
-D = [0,0
-     0,0];
+A = [0 0 1 0
+     0 0 0 1
+     0 0 0 0
+     0 0 0 0];
+
+B = [0 0
+     0 0
+     1 0
+     0 1];
+
+C = [1 0 0 0
+     0 1 0 0];
+
+D = [0 0
+     0 0];
+
 sys = ss(A,B,C,D);
 
 
@@ -38,10 +34,10 @@ sys = ss(A,B,C,D);
 % Reference
 %
 r0 = zeros(2,1);
-r = [3;6];
+r = [7;11];
 
-v0 = r0;
-Kappa = 100;
+v0 = zeros(2,1);
+kappa = 100;
 
 
 %
@@ -61,19 +57,18 @@ Kd = 2*zeta*wn;
 %
 % Nav Fields
 %
+
 %constraint Av+b>=0
-constr.A = [1,0;
-            -1,0;
-            0,1;
-            0,-1;
-            -1,-1];
-constr.b = [10;10;10;10;4];
+%for diagonal
+%  A <- -1 -1
+%  B <- x/y intercept
+constr.A = [ 0 -1 ];
 
+constr.b = [10];
 
-
-zeta = 2;
-delta = 0;
-eta = 1;
+zeta = 2; %influence margin
+delta = -100; %static safety margin
+eta = 1; %...
 
 
 %
@@ -81,56 +76,64 @@ eta = 1;
 %
 out = sim("erg");
 
-tu = out.u.Time;
-t = out.x.Time;
-u1 = out.u.Data(1,:);
-u2 = out.u.Data(2,:);
-x = out.x.Data(1,:)';
-y = out.x.Data(2,:)';
-v = out.v.Data;
+data.tu = out.u.Time;
+data.t = out.x.Time;
+data.u1 = out.u.Data(1,:);
+data.u2 = out.u.Data(2,:);
+data.x = out.x.Data(1,:)';
+data.y = out.x.Data(2,:)';
+data.r = out.r.Data;
+data.v = out.v.Data;
+data.rho_att = out.rho_att.Data;
+data.rho_rep_wall = out.rho_rep_wall.Data;
+data.rho_rep_corr = out.rho_rep_corr.Data;
 
+%Check Control Inputs
 figure
-plot(tu,u1)
+plot(data.tu,data.u1)
 hold on
-plot(tu,u2)
+plot(data.tu,data.u2)
 hold off
 grid on
 title('Control Inputs')
 
+%
 figure
-% scatter(x,y,[],t,'.')
-% scatter(v(1,:),v(2,:),[],t,'.')
-plot(v(1,:),v(2,:))
-axis([0,10,0,10])
+plot(data.r(:,1),data.r(:,2),'r*')
+hold on
+plot(data.v(1,:),data.v(2,:),'b.')
+hold off
+axis([0,12,0,12])
 xlabel('x pos')
 ylabel('y pos')
-grid on
-xline(10,'r--')
-xline(10-delta,'y--')
-xline(10-zeta,'g--')
+grid on, zoom on
+% xline(10,'r--')
+% % xline(10-delta,'y--')
+% xline(10-zeta,'y--')
 yline(10,'r--')
-yline(10-delta,'y--')
-yline(10-zeta,'g--')
-line([4;0],[0;4],'linestyle','--','color','r')
-line([4;0],[0;4],'linestyle','--','color','r')
+% yline(10-delta,'y--')
+yline(10-zeta,'y--')
+legend('r','v')
 title('v')
 
-r
-[x(end) y(end)]
-[v(1,end) v(2,end)]
+%
+norm_rho_att = [];
+norm_rho_rep_wall = [];
+norm_rho_rep_corr = [];
+for i = 1:length(data.rho_att)
+     norm_rho_att = [norm_rho_att norm(data.rho_att(:,i))];
+     norm_rho_rep_wall = [norm_rho_rep_wall norm(data.rho_rep_wall(:,i))];
+     norm_rho_rep_corr = [norm_rho_rep_corr norm(data.rho_rep_corr(:,i))];
+end
+
+figure
+plot(data.t,norm_rho_att, data.t,norm_rho_rep_wall, data.t,norm_rho_rep_corr)
+legend('\rho_{att}','\rho_{repwall}','\rho_{repcorr}')
+title('rho vals over time')
 
 
-return
-
-% %
-% % symplectic euler
-% %
-% x0 = 0;
-% dx0 = 0;
-% eul.x = [x0];
-% eul.dx = [dx0];
-% for k = 1:10
-%     eul.x(:,k) = 
-% end
+%print some values for quick verification
+[data.x(end) data.y(end)]
+[data.v(1,end) data.v(2,end)]
 
 
